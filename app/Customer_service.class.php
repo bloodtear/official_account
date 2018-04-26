1189b3b1875058a1e23a80ebd16187588de52e00
bloodtear <xiaoyu1120520@126.com>
2018-04-26 17:04:16 +0800

    debug

--- app/Customer_service.class.php
+++ app/Customer_service.class.php
@@ -8,14 +8,14 @@ class Customer_service {
         $json = self::makeTextJson(
             $openid, 
             "[欢迎消息]欢迎您的关注，关注此公众号小程序便不会有消息的限制，公众号会发送小程序相关的消息");
-        return = Wxapi::send_text_msg($json);
+        return Wxapi::send_text_msg($json);
     }
     
     public static function autoReply($openid) {
         $json = self::makeTextJson(
             $openid, 
             "[自动回复]欢迎您的关注，关注此公众号小程序便不会有消息的限制，公众号会发送小程序相关的消息");
-        return = Wxapi::send_text_msg($json);
+        return Wxapi::send_text_msg($json);
     }
 
 
