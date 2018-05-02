edc35fcbf88e06e7245fa8960d0dd1486dafd38d
bloodtear <xiaoyu1120520@126.com>
2018-05-02 20:23:52 +0800

    修改公众号首次进入的消息提示

--- app/Customer_service.class.php
+++ app/Customer_service.class.php
@@ -7,7 +7,7 @@ class Customer_service {
     public static function welcomeMsg($openid) {
         $json = self::makeTextJson(
             $openid, 
-            "[欢迎消息]欢迎您的关注，关注此公众号小程序便不会有消息的限制，公众号会发送小程序相关的消息");
+            "由于小程序加了诸多限制，比如只能发一次通知，超过7天也没法通知等等。关注这里可以不受限制的获得活动相关通知！");
         return Wxapi::send_text_msg($json);
     }
     
